194c8acdd8e69dde2c724a1d3a0897850e7b1b81
AnneY <evangeline-lun@foxmail.com>
2022-10-27 23:46:56 +0800

    test: add tests

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,103 @@
+import operator
+
+import numpy as np
+import pytest
+
+from docarray import Document, DocumentArray
+from jina import Flow
+
+from executor import RedisIndexer
+
+# def test_flow(docker_compose):
+#     f = Flow().add(
+#         uses=RedisIndexer,
+#         uses_with={'index_name': 'test1', 'n_dim': 2},
+#     )
+
+#     with f:
+#         f.post(
+#             on='/index',
+#             inputs=[
+#                 Document(id='a', embedding=np.array([1, 3])),
+#                 Document(id='b', embedding=np.array([1, 1])),
+#                 Document(id='c', embedding=np.array([3, 1])),
+#                 Document(id='d', embedding=np.array([2, 3])),
+#             ],
+#         )
+
+#         docs = f.post(
+#             on='/search',
+#             inputs=[Document(embedding=np.array([1, 1]))],
+#         )
+#         assert docs[0].matches[0].id == 'b'
+
+
+# def test_reload_keep_state():
+#     docs = DocumentArray([Document(embedding=np.random.rand(3)) for _ in range(2)])
+#     f = Flow().add(
+#         uses=RedisIndexer,
+#         uses_with={'index_name': 'test2', 'n_dim': 3},
+#     )
+
+#     with f:
+#         f.index(docs)
+#         first_search = f.search(inputs=docs)
+
+#     with f:
+#         second_search = f.search(inputs=docs)    
+#         assert len(first_search[0].matches) == len(second_search[0].matches)
+
+
+# numeric_operators_redis = {
+#     'gte': operator.ge,
+#     'gt': operator.gt,
+#     'lte': operator.le,
+#     'lt': operator.lt,
+#     'eq': operator.eq,
+#     'ne': operator.ne,
+# }
+
+
+# @pytest.mark.parametrize(
+#     'filter_gen,operator',
+#     [
+#         (lambda threshold: f'@price:[{threshold} inf] ', 'gte'),
+#         (lambda threshold: f'@price:[({threshold} inf] ', 'gt'),
+#         (lambda threshold: f'@price:[-inf {threshold}] ', 'lte'),
+#         (lambda threshold: f'@price:[-inf ({threshold}] ', 'lt'),
+#         (lambda threshold: f'@price:[{threshold} {threshold}] ', 'eq'),
+#         (lambda threshold: f'(- @price:[{threshold} {threshold}]) ', 'ne'),
+#     ]
+# )
+    
+# def test_filtering(filter_gen, operator, docker_compose):
+#     n_dim = 3
+
+#     f = Flow().add(
+#         uses=RedisIndexer,
+#         uses_with={
+#             'n_dim': n_dim,
+#             'columns': {'price': 'float'},
+#         },
+#     )
+
+#     docs = DocumentArray(
+#         [
+#             Document(id=f'r{i}', embedding=np.random.rand(n_dim), tags={'price': i})
+#             for i in range(50)
+#         ]
+#     )
+#     with f:
+#         f.index(docs)
+
+#         for threshold in [10, 20, 30]:
+
+#             filter_ = filter_gen(threshold)
+#             doc_query = DocumentArray([Document(embedding=np.random.rand(n_dim))])
+#             indexed_docs = f.search(doc_query, parameters={'filter': filter_})
+
+#             assert len(indexed_docs[0].matches) > 0
+            
+#             assert all(
+#                 [numeric_operators_redis[operator](r.tags['price'], threshold) for r in indexed_docs[0].matches]
+#             )
---
+++ tests/unit/test_executor.py
@@ -0,0 +1,193 @@
+import os
+
+import pytest
+from docarray.array.redis import DocumentArrayRedis
+from docarray import Document, DocumentArray
+
+import numpy as np
+
+from executor import RedisIndexer
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+compose_yml = os.path.abspath(os.path.join(cur_dir, '../docker-compose.yml'))
+
+
+def assert_document_arrays_equal(arr1, arr2):
+    assert len(arr1) == len(arr2)
+    for d1, d2 in zip(arr1, arr2):
+        assert d1.id == d2.id
+        assert d1.content == d2.content
+        assert d1.chunks == d2.chunks
+        assert d1.matches == d2.matches
+
+
+@pytest.fixture
+def docs():
+    return DocumentArray(
+        [
+            Document(id='doc1', embedding=np.random.rand(128)),
+            Document(id='doc2', embedding=np.random.rand(128)),
+            Document(id='doc3', embedding=np.random.rand(128)),
+            Document(id='doc4', embedding=np.random.rand(128)),
+            Document(id='doc5', embedding=np.random.rand(128)),
+            Document(id='doc6', embedding=np.random.rand(128)),
+        ]
+    )
+
+
+@pytest.fixture
+def update_docs():
+    return DocumentArray(
+        [
+            Document(id='doc1', text='modified', embedding=np.random.rand(128)),
+        ]
+    )
+
+
+def test_init(docker_compose):
+    indexer = RedisIndexer(index_name='test')
+
+    assert isinstance(indexer._index, DocumentArrayRedis)
+    assert indexer._index._config.index_name == 'test'
+    assert indexer._index._config.port == 6379
+
+
+def test_index(docs, docker_compose):
+    indexer = RedisIndexer(index_name='test1')
+    indexer.index(docs)
+
+    assert len(indexer._index) == len(docs)
+
+
+def test_delete(docs, docker_compose):
+    indexer = RedisIndexer(index_name='test2')
+    indexer.index(docs)
+
+    ids = ['doc1', 'doc2', 'doc3']
+    indexer.delete({'ids': ids})
+    assert len(indexer._index) == len(docs) - 3
+    for doc_id in ids:
+        assert doc_id not in indexer._index
+
+
+def test_update(docs, update_docs, docker_compose):
+    # index docs first
+    indexer = RedisIndexer(index_name='test3')
+    indexer.index(docs)
+    assert_document_arrays_equal(indexer._index, docs)
+
+    # update first doc
+    indexer.update(update_docs)
+    assert indexer._index[0].id == 'doc1'
+    assert indexer._index['doc1'].text == 'modified'
+
+
+def test_fill_embeddings(docker_compose):
+    indexer = RedisIndexer(index_name='test4', distance='L2', n_dim=1)
+
+    indexer.index(DocumentArray([Document(id='a', embedding=np.array([1]))]))
+    search_docs = DocumentArray([Document(id='a')])
+    indexer.fill_embedding(search_docs)
+    assert search_docs['a'].embedding is not None
+    assert (search_docs['a'].embedding == np.array([1])).all()
+
+    with pytest.raises(KeyError, match='b'):
+        indexer.fill_embedding(DocumentArray([Document(id='b')]))
+
+
+def test_filter(docker_compose):
+    docs = DocumentArray.empty(5)
+    docs[0].text = 'hello'
+    docs[1].text = 'world'
+    docs[2].tags['x'] = 0.3
+    docs[2].tags['y'] = 0.6
+    docs[3].tags['x'] = 0.8
+
+    indexer = RedisIndexer(index_name='test5', columns={'text': 'str', 'x': 'float'})
+    indexer.index(docs)
+
+    result = indexer.filter(parameters={'filter': 'hello'})
+    assert len(result) == 1
+    assert result[0].text == 'hello'
+
+    result = indexer.filter(parameters={'filter': '@x:[(0.5 inf] '})
+    assert len(result) == 1
+    assert result[0].tags['x'] == 0.8
+
+
+# def test_persistence(docs, docker_compose):
+#     indexer1 = RedisIndexer(index_name='test6', distance='L2')
+#     indexer1.index(docs)
+#     indexer2 = RedisIndexer(index_name='test6', distance='L2')
+#     assert_document_arrays_equal(indexer2._index, docs)
+
+
+@pytest.mark.parametrize(
+    'metric, metric_name',
+    [('L2', 'euclid_similarity'), ('COSINE', 'cosine_similarity')],
+)
+def test_search(metric, metric_name, docs, docker_compose):
+    # test general/normal case
+    indexer = RedisIndexer(index_name='test7', distance=metric)
+    indexer.index(docs)
+    query = DocumentArray([Document(embedding=np.random.rand(128)) for _ in range(10)])
+    indexer.search(query)
+
+    for doc in query:
+        similarities = [t[metric_name].value for t in doc.matches[:, 'scores']]
+        assert sorted(similarities, reverse=True) == similarities
+
+
+@pytest.mark.parametrize('limit', [1, 2, 3])
+def test_search_with_match_args(docs, limit, docker_compose):
+    indexer = RedisIndexer(index_name='test8', match_args={'limit': limit})
+    indexer.index(docs)
+    assert 'limit' in indexer._match_args.keys()
+    assert indexer._match_args['limit'] == limit
+
+    query = DocumentArray([Document(embedding=np.random.rand(128))])
+    indexer.search(query)
+
+    assert len(query[0].matches) == limit
+
+    docs[0].tags['text'] = 'hello'
+    docs[1].tags['text'] = 'world'
+    docs[2].tags['text'] = 'hello'
+
+    indexer = RedisIndexer(
+        index_name='test9',
+        columns={'text': 'str'},
+        match_args={
+            'filter': '@text:hello',
+            'limit': 1,
+        },
+    )
+    indexer.index(docs)
+
+    indexer.search(query)
+    assert len(query[0].matches) == 1
+    assert query[0].matches[0].tags['text'] == 'hello'
+    
+
+def test_clear(docs, docker_compose):
+    indexer = RedisIndexer(index_name='test10')
+    indexer.index(docs)
+    assert len(indexer._index) == 6
+    indexer.clear()
+    assert len(indexer._index) == 0
+
+
+def test_columns(docker_compose):
+    n_dim = 3
+    indexer = RedisIndexer(
+        index_name='test11', n_dim=n_dim, columns={'price': 'float'}
+    )
+
+    docs = DocumentArray(
+        [
+            Document(id=f'r{i}', embedding=i * np.ones(n_dim), tags={'price': i})
+            for i in range(10)
+        ]
+    )
+    indexer.index(docs)
+    assert len(indexer._index) == 10
